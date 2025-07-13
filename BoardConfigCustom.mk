f16b0f4b0b8a64700a9a1229aecb97f29850899c
Khaliq <bias.khaliq@gmail.com>
2025-07-13 04:34:08 +0000

    Initial Android One For Sunfish

---
+++ BoardConfigCustom.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2020-2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Kernel
+BOARD_KERNEL_IMAGE_NAME := Image.lz4
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_CONFIG := sunfish_defconfig
+TARGET_KERNEL_SOURCE := kernel/google/msm-4.14
+TARGET_NEEDS_DTBOIMAGE := true
+
+# Manifests
+DEVICE_MANIFEST_FILE += device/google/sunfish/aosp_manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/aosp/config/device_framework_matrix.xml
+
+# Partitions
+AB_OTA_PARTITIONS += \
+    vendor
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# Reserve space for gapps install
+-include vendor/aosp/config/BoardConfigReservedSize.mk
+
+# SELinux
+BOARD_SEPOLICY_DIRS += device/google/sunfish/sepolicy-aosp/dynamic
+BOARD_SEPOLICY_DIRS += device/google/sunfish/sepolicy-aosp/vendor
+
+# Verified Boot
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+
+include vendor/google/sunfish/BoardConfigVendor.mk
